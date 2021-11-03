de792ea610a85df5db6e943da38f95d17018e5ae
GimenaRivoira <gimenarivoira@gmail.com>
2021-11-03 20:28:09 -0300

    fixed errors

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import layout1 from '../../images/layoutfinal.jpg'
-import layout2 from '../../images/layout.jpg'
 
 const Carousel = () => {
     return (
@@ -9,9 +8,6 @@ const Carousel = () => {
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src={layout1} alt="First slide" />
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src={layout2} alt="Second slide" />
     </div>
   </div>
 </div>
